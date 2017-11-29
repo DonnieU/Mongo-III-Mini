461b4fe1c3b397f20d812cd1805eeaae200d4b6b
Biff <thedonnieutley@gmail.com>
2017-11-29 14:15:49 -0600

    first commit; some parts working

---
+++ api/controllers/postsController.js
@@ -0,0 +1,102 @@
+const mongoose = require('mongoose');
+
+const Post = require('../models/post');
+const Comment = require('../models/comment');
+
+const STATUS_USER_ERROR = 422;
+
+/* Fill in each of the below controller methods */
+const createPost = (req, res) => {
+  const { title, text } = req.body;
+  const newPost = new Post({ title, text });
+  // newPost.save((err, createdPost) => {
+  //   if (err) {
+  //     res.status(STATUS_USER_ERROR);
+  //     res.json(err);
+  //     return;
+  //   }
+  //   res.json(createdPost);
+  // });
+
+  // Promise-like thanks to Mongoose...
+  newPost
+    .save()
+    .then((createdPost) => {
+      res.json(createdPost);
+    })
+    .catch((err) => {
+      res
+        .status(STATUS_USER_ERROR)
+        .json({ errorMessage: err.message });
+    });
+};
+
+const listPosts = (req, res) => {
+  // Post.find({}, (err, posts) => {
+  //   if (err) {
+  //     res.status(500).json(err);
+  //     return;
+  //   }
+  //   res.json(posts);
+  // });
+  // this way returns the text of the comment, not just the comment id...
+  Post.find({})
+    .populate('comments', 'text')
+    .exec()
+    .then((posts) => {
+      res.json(posts);
+    })
+    .catch((err) => {
+      res.status(STATUS_USER_ERROR).json(err);
+    });
+};
+
+const findPost = (req, res) => {
+
+};
+
+const addComment = (req, res) => {
+  const { id } = req.params;
+  const { text } = req.body;
+  const newComment = new Comment({ _parent: id, text });
+  newComment
+    .save()
+    .then((comment) => {
+      Post.findById(id, (postErr, post) => {
+        if (postErr || post === null) {
+          res.status(STATUS_USER_ERROR).json({ message: `post not found at id ${id}` });
+          return;
+        }
+        post.comments.push(comment);
+        post.save();
+        res.json({ success: 'comment added!' });
+      });
+    })
+    .catch((err) => {
+      res.status(STATUS_USER_ERROR).json({ errorMessage: err.message });
+    });
+};
+
+// In this function, we need to delete the comment document
+// We also need to delete the comment's parent post's reference
+// to the comment we just deleted
+const deleteComment = (req, res) => {
+
+};
+
+// Similarly, in this function we need to delete the post document,
+// along with any comments that are the children of this post
+// We don't want any orphaned children in our database
+const deletePost = (req, res) => {
+
+};
+
+module.exports = {
+  createPost,
+  listPosts,
+  findPost,
+  addComment,
+  deleteComment,
+  deletePost
+};
+
